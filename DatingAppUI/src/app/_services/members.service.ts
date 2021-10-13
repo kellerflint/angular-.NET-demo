8e8003dd911abbba5f40357cfb186131995b4c3b
Keller Flint <kflint0068@gmail.com>
2021-10-13 15:09:15 -0700

    added pictures and probably some other stuff

--- DatingAppUI/src/app/_services/members.service.ts
+++ DatingAppUI/src/app/_services/members.service.ts
@@ -13,10 +13,10 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(`${this.baseUrl}users`)
+    return this.http.get<Member[]>(`${this.baseUrl}users`);
   }
 
   getMember(username: string) {
-    return this.http.get<Member>('this.baseUrl}users/${username}');
+    return this.http.get<Member>(`${this.baseUrl}users/${username}`);
   }
 }
